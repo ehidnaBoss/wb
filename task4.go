f923741f4dcfe210cc6f6e916a9ca73185df796d
gojiingl(salfetka) <gojiingl@student.21-school.ru>
2024-08-14 18:11:49 +0300

    fixed data race

--- task4.go
+++ task4.go
@@ -9,28 +9,34 @@ import (
 )
 
 func main() {
-
 	mainChan := make(chan int)
-
 	var N int
+
 	fmt.Print("Введите кол-во воркеров: ")
 	_, err := fmt.Scan(&N)
+
 	if err != nil {
 		fmt.Println("АШИПКА", err)
 		return
 	}
-
 	wg := sync.WaitGroup{}
 
 	for i := 1; i < N; i++ {
 		wg.Add(1)
 		go worker(i, mainChan, &wg)
 	}
+
 	go func() {
 		for i := 1; ; i++ {
 			mainChan <- i
 		}
 	}()
+
+	go func() {
+		wg.Wait()
+		close(mainChan)
+	}()
+
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 
@@ -38,19 +44,15 @@ func main() {
 
 	fmt.Println("Получени сигнал завершения...")
 
-	close(mainChan)
-
-	wg.Wait()
-
 	fmt.Println("Воркеры больше не воркают!")
 }
 
 func worker(id int, channel <-chan int, wg *sync.WaitGroup) {
 	defer wg.Done()
+
 	for j := range channel {
 		fmt.Println("worker", id, "started  job", j)
 		//time.Sleep(time.Second)
 		fmt.Println("worker", id, "finished job", j)
-
 	}
 }
