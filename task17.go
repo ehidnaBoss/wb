83600e9aa7cf8de624a227dd22801ae5f143fa8b
gojiingl(salfetka) <gojiingl@student.21-school.ru>
2024-08-16 02:57:00 +0300

    t17

---
+++ task17.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	arr := [10]int{2, 4, 6, 8, 9, 10}
+	target := 4
+
+	index := sort.Search(len(arr), func(i int) bool {
+		return arr[i] >= target
+	})
+
+	if index < len(arr) && arr[index] == target {
+		fmt.Printf("Элемент %d найден на позиции %d\n", target, index)
+	} else {
+		fmt.Printf("Нет такого %d", target)
+	}
+}
